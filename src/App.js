7e99f21ca4a2f268c3c5cea545eca3555daac64f
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2018-11-27 09:11:28 -0500

    w6d2

--- src/App.js
+++ src/App.js
@@ -1,28 +1,81 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const Message = ({ message: { username, content } }) => (
+  <div className="message">
+    <span className="username">{username}</span>
+    <span className="content">{content}</span>
+  </div>
+);
+
+const MessageList = ({ messages }) => (
+  <div className="message-list">
+    {messages.map((m, i) => (
+      <Message key={i} message={m} />
+    ))}
+  </div>
+);
+
+const ChatBar = ({ currentUser: { name }, setUsername, addMessage }) => {
+  const [messageInput, setMessageInput] = useState('');
+  return (
+    <div className="chat-bar">
+      <input
+        className="username"
+        value={name}
+        onChange={({ target: { value } }) => setUsername(value)}
+      />
+      <input
+        className="message"
+        placeholder="Type a message and hit ENTER"
+        value={messageInput}
+        onChange={({ target: { value } }) => setMessageInput(value)}
+        onKeyUp={e => {
+          if (e.keyCode === 13) {
+            addMessage(messageInput);
+            setMessageInput('');
+          }
+        }}
+      />
+    </div>
+  );
+};
+
+const Header = () => (
+  <div className="header">
+    <h2>Chatty</h2>
+  </div>
+);
+
+const App = () => {
+  const [currentUser, setCurrentUser] = useState({
+    name: 'Bob'
+  });
+  const [messages, setMessages] = useState([
+    {
+      username: 'Bob',
+      content: 'Has anyone seen my marbles?'
+    },
+    {
+      username: 'Anonymous',
+      content:
+        'No, I think you lost them. You lost your marbles Bob. You lost them for good.'
+    }
+  ]);
+
+  return (
+    <div className="app">
+      <Header />
+      <MessageList {...{ messages }} />
+      <ChatBar
+        {...{ currentUser }}
+        setUsername={name => setCurrentUser({ ...currentUser, name })}
+        addMessage={content => {
+          setMessages([...messages, { username: currentUser.name, content }]);
+        }}
+      />
+    </div>
+  );
+};
 
 export default App;
